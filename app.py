6fca59510023a02db0897591cb99b679ce15ee75
Riyan Adi Saputro <75431588+Riyanadi120802@users.noreply.github.com>
2024-02-21 00:39:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import torch
 from PIL import Image
 from torchvision import transforms
+from torchvision import datasets, models, transforms
+import torch.nn as nn
 import torch.nn.functional as F
 
 # Define YourModel class
@@ -26,50 +28,74 @@ class Model(nn.Module):
         return self.model.forward(x)
 
 # Load model
-model = Model() # Replace with your model definition
-model.load_state_dict(torch.load('Adam_00001_2_dataCampuran.pt', map_location=torch.device('cpu'))) # Replace with your model file name and adjust for the device used
+model = Model() # Ganti dengan definisi model Anda
+model.load_state_dict(torch.load('Adam_00001_2_dataCampuran.pt', map_location=torch.device('cpu'))) # Ganti dengan nama file model Anda dan sesuaikan dengan perangkat yang digunakan
 model.eval()
 
 # Define transform
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# Define UI
-st.title('Deploying PyTorch Object Detection Model to Streamlit')
+# Tentukan perangkat
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Define prediction function
-def object_detection(model, image_transforms, image_path):
+labels_data ={
+    0: "Layak",
+    1: "Tidak Layak"
+}
+
+def image_pred(model, image_transforms, image_path, labels_data):
     image = Image.open(image_path).convert('RGB')
-    image = image_transforms(image).unsqueeze(0)
+
+    resize_image = image.resize((128,128))
+
+    image = image_transforms(image).float()
+    image = image.unsqueeze(0).to(device)
 
     output = model(image)
-    # Process output to get detected objects and their coordinates
-    # Modify this part according to your object detection model output format
-    # For example, if using Faster R-CNN, you can parse the bounding box coordinates and class labels from the output
-    
-    return detected_objects
+    probs = F.softmax(output, dim=1)
+    conf, classes = torch.max(probs, 1)
 
-# Choose between camera or upload image
-option = st.selectbox('Choose image source:', ('Camera', 'Upload Image'))
+    return labels_data[classes.item()], round(conf.item(), 3), resize_image
 
-if option == 'Camera':
-    st.write('Please take a picture')
+# Define UI
+st.title('Deploying PyTorch Model to Streamlit')
+
+# Pilihan menggunakan kamera atau unggah gambar
+option = st.selectbox('Pilih sumber gambar:', ('Kamera', 'Unggah Gambar'))
+
+if option == 'Kamera':
+    st.write('Silakan ambil gambar')
     picture = st.camera_input("Take a picture")
     if picture is not None:
-        detected_objects = object_detection(model, transform, picture)
-        # Display image with bounding boxes
-        # Modify this part to draw bounding boxes on the image
-        st.image(picture, caption='Detected Objects')
-        # Display detected objects and their coordinates
-        st.write(detected_objects)
+        predicted_class, confidence, resized_image = image_pred(model, transform, picture, labels_data)
+        st.image(resized_image)
+        st.write('Predicted Class:', predicted_class)
+        st.write('Confidence:', confidence)
+        # Tampilkan progress bar untuk confidence
+        progress_bar = st.progress(confidence)
+        
+        # Menampilkan notifikasi berdasarkan prediksi
+        if predicted_class == "Layak":
+            st.success("Selamat! Anda mendapatkan 1000 koin🪙 sebagai reward karena botol ini layak untuk dijadikan kerajinan!")
+        else:
+            st.warning("Botol ini tidak layak untuk dijadikan kerajinan, Anda mendapatkan 100 koin🪙.")
 else:
     uploaded_file = st.file_uploader('Upload Image', type=['png', 'jpg', 'jpeg'])
     if uploaded_file is not None:
-        detected_objects = object_detection(model, transform, uploaded_file)
-        # Display image with bounding boxes
-        # Modify this part to draw bounding boxes on the image
-        st.image(uploaded_file, caption='Detected Objects')
-        # Display detected objects and their coordinates
-        st.write(detected_objects)
+        predicted_class, confidence, resized_image = image_pred(model, transform, uploaded_file, labels_data)
+        st.image(resized_image)
+        st.write('Predicted Class:', predicted_class)
+        
+        st.write('Confidence:', confidence)
+        # Tampilkan progress bar untuk confidence
+        progress_bar = st.progress(confidence)
+        
+        # Menampilkan notifikasi berdasarkan prediksi
+        if predicted_class == "Layak":
+            st.success("Selamat! Anda mendapatkan 1000 koin🪙 sebagai reward karena botol ini layak untuk dijadikan kerajinan!")
+        else:
+            st.warning("Botol ini tidak layak untuk dijadikan kerajinan, Anda mendapatkan 100 koin🪙.")
